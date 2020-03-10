ce39f7f0c646a34a3f23ee1e2525d2579d699487
fachrul ariestyan <ariestyann@gmail.com>
2020-03-10 21:03:40 +0700

    Membenarkan sedikit tipo pada max dan min

--- Kelas-Terbuka/05 - Tipe Data Primitive/src/com/tutorial/Main.java
+++ Kelas-Terbuka/05 - Tipe Data Primitive/src/com/tutorial/Main.java
@@ -21,7 +21,7 @@ public class Main {
         System.out.println("========BYTE========");
         System.out.println("Nilai byte b = " + b);
         System.out.println("Nilai max = " + Byte.MAX_VALUE);
-        System.out.println("Nilai max = " + Byte.MIN_VALUE);
+        System.out.println("Nilai min = " + Byte.MIN_VALUE);
         System.out.println("Besar Memory = " + Byte.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Byte.SIZE + " Bit");
 
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("========SHORT========");
         System.out.println("Nilai short s = " + s);
         System.out.println("Nilai max = " + Short.MAX_VALUE);
-        System.out.println("Nilai max = " + Short.MIN_VALUE);
+        System.out.println("Nilai min = " + Short.MIN_VALUE);
         System.out.println("Besar Memory = " + Short.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Short.SIZE + " Bit");
 
@@ -39,7 +39,7 @@ public class Main {
         System.out.println("========INTEGER========");
         System.out.println("Nilai integer i = " + i);
         System.out.println("Nilai max = " + Integer.MAX_VALUE);
-        System.out.println("Nilai max = " + Integer.MIN_VALUE);
+        System.out.println("Nilai min = " + Integer.MIN_VALUE);
         System.out.println("Besar Memory = " + Integer.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Integer.SIZE + " Bit");
 
@@ -48,7 +48,7 @@ public class Main {
         System.out.println("========LONG========");
         System.out.println("Nilai long l = " + l);
         System.out.println("Nilai max = " + Long.MAX_VALUE);
-        System.out.println("Nilai max = " + Long.MIN_VALUE);
+        System.out.println("Nilai min = " + Long.MIN_VALUE);
         System.out.println("Besar Memory = " + Long.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Long.SIZE + " Bit");
 
@@ -57,7 +57,7 @@ public class Main {
         System.out.println("========DOUBLE========");
         System.out.println("Nilai double d = " + d);
         System.out.println("Nilai max = " + Double.MAX_VALUE);
-        System.out.println("Nilai max = " + Double.MIN_VALUE);
+        System.out.println("Nilai min = " + Double.MIN_VALUE);
         System.out.println("Besar Memory = " + Double.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Double.SIZE + " Bit");
 
@@ -66,7 +66,7 @@ public class Main {
         System.out.println("========FLOAT========");
         System.out.println("Nilai float f = " + f);
         System.out.println("Nilai max = " + Float.MAX_VALUE);
-        System.out.println("Nilai max = " + Float.MIN_VALUE);
+        System.out.println("Nilai min = " + Float.MIN_VALUE);
         System.out.println("Besar Memory = " + Float.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Float.SIZE + " Bit");
 
@@ -75,7 +75,7 @@ public class Main {
         System.out.println("========CHARACTER========");
         System.out.println("Nilai char c = " + c);
         System.out.println("Nilai max = " + Character.MAX_VALUE);
-        System.out.println("Nilai max = " + Character.MIN_VALUE);
+        System.out.println("Nilai min = " + Character.MIN_VALUE);
         System.out.println("Besar Memory = " + Character.BYTES + " Bytes");
         System.out.println("Besar Memory = " + Character.SIZE + " Bit");
 
@@ -84,7 +84,7 @@ public class Main {
         System.out.println("========BOOLEAN========");
         System.out.println("Nilai boolean val = " + val);
         System.out.println("Nilai max = " + true);
-        System.out.println("Nilai max = " + false);
+        System.out.println("Nilai min = " + false);
 
     }
 }
